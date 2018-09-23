def76297229014bd297372be8644e0b29a249c29
Andre Raats <andre.raats@minbzk.nl>
2018-09-23 20:07:54 +0200

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#37tlkmq1o=^jhq9v$$=_43&jf7_eeds2u0=(#*6!5q=g9ssyk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
