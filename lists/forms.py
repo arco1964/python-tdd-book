6ae816983a8c9ab29ec6792e2da70719a08d5e7d
Andre Raats <andre.raats@gmail.com>
2018-10-20 16:06:17 +0200

    end of chapter 19 - 2

--- lists/forms.py
+++ lists/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from lists.models import Item
 from django.core.exceptions import ValidationError
+
+from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -20,6 +21,7 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
+
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
@@ -31,6 +33,7 @@ class ExistingListItemForm(ItemForm):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
 
+
     def validate_unique(self):
         try:
             self.instance.validate_unique()
@@ -38,5 +41,8 @@ class ExistingListItemForm(ItemForm):
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
 
+
+
     def save(self):
         return forms.models.ModelForm.save(self)
+
